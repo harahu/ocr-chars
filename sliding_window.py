ff3e2dd5e8a150a75e0f9cb84b5265f9e62eac53
Aleksander Wasaznik <aleksander@wasaznik.com>
2017-04-27 20:03:21 +0200

    Fix one-off error in sliding-window

--- sliding_window.py
+++ sliding_window.py
@@ -4,5 +4,5 @@ def sliding_window_patches(img):
     """Create an array of contents of a 20x20 window sliding over img with
     stride 1 in both directions."""
     patches = extract_patches_2d(img, (20,20))
-    patches = patches.reshape((img.shape[0] - 20) * (img.shape[1] - 20), 20 * 20)
+    patches = patches.reshape((img.shape[0] - 19) * (img.shape[1] - 19), 20 * 20)
     return patches
