9260a37a089f09c276925d518a6aa5d94c799eb7
Aleksander Wasaznik <aleksander@wasaznik.com>
2017-04-27 18:25:27 +0200

    Change the function in sliding_window.py to match classify_chars

--- sliding_window.py
+++ sliding_window.py
@@ -1,8 +1,8 @@
-from itertools import product
 from sklearn.feature_extraction.image import extract_patches_2d
-import numpy as np
 
-def map_over_windows(func, img):
+def sliding_window_patches(img):
+    """Create an array of contents of a 20x20 window sliding over img with
+    stride 1 in both directions."""
     patches = extract_patches_2d(img, (20,20))
-    patches = patches.reshape(img.shape[0] - 20, img.shape[1] - 20, 20 * 20)
-    return np.apply_along_axis(func, 2, patches)
+    patches = patches.reshape((img.shape[0] - 20) * (img.shape[1] - 20), 20 * 20)
+    return patches
